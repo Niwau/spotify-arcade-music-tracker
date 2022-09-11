c8caac7162460549eca49d05114e14a398efb218
Guilherme Ribeiro <xxguilhermesoaresxx@gmail.com>
2022-09-11 18:34:54 -0300

    Arrumando rotas para o deploy

--- additional.d.ts
+++ additional.d.ts
@@ -2,7 +2,8 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             CLIENT_ID: string,
-
+            CLIENT_SECRET: string,
+            URL: string
         }
     }
 }
--- utils/spotify.ts
+++ utils/spotify.ts
@@ -1,7 +1,7 @@
 type spotifyType = {
     client_id: string,
     response_type: 'token',
-    redirect_uri: 'http://localhost:3000/tracks'
+    redirect_uri: process.env.URL
     endpoint: 'https://accounts.spotify.com/authorize'
     scope: string
 }
@@ -9,7 +9,7 @@ type spotifyType = {
 export const spotify : spotifyType = {
     client_id: process.env.CLIENT_ID,
     response_type: 'token',
-    redirect_uri: 'http://localhost:3000/tracks',
+    redirect_uri: 'https://spotify-arcade-music-tracker.vercel.app/tracks',
     endpoint: 'https://accounts.spotify.com/authorize',
     scope: 'user-top-read'
 }
